48514fee52356b7f1b01bc8183a2c7b25f73b4b3
MarkSakhar <46299529+Marik2k4@users.noreply.github.com>
2020-09-12 01:55:37 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   CONFIRMATION: "770d947c",
   TOKEN: "f6e62e3807ce3e09cf607a8fb4bd8880e9c7c081eaa83cb97f992ccc6e9e5dc6c8547bb84e766d118b5ae"
 };
